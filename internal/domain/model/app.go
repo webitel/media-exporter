72e784aaeed52e419cdc50514d0193fb138b2541
kirychukyurii <y.kirychuk@webitel.com>
2026-07-21 20:17:39 +0300

    refactor(version): inject CurrentVersion via ldflags, drop static version slice

--- internal/domain/model/app.go
+++ internal/domain/model/app.go
@@ -5,17 +5,4 @@ const (
 	NamespaceName  = "webitel"
 )
 
-var versions = []string{
-	"26.04",
-	"26.02",
-	"25.12",
-	"25.10",
-	"25.08",
-	"25.06",
-	"25.04",
-	"25.02",
-}
-
-var (
-	CurrentVersion = versions[0]
-)
+var CurrentVersion = "dev"
